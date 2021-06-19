da851193b28bffe2884c622868bd55b071efe3e0
Tambapps <tambapps@gmail.com>
2021-06-20 01:01:27 +0200

    TODO

--- core/lib/src/util.dart
+++ core/lib/src/util.dart
@@ -18,6 +18,8 @@ Future<InternetAddress> toAddress(address) async {
 
 /// doesn't work on android according to https://stackoverflow.com/questions/52411168/how-to-get-device-ip-in-dart-flutter
 /// use WifiFlutter instead for android
+// TODO improve me like in fandem
+// TODO also work on android so just rename it getIpAddress
 Future<InternetAddress> getDesktopIpAddress() async {
   final addresses = (await NetworkInterface.list(includeLoopback: false, includeLinkLocal: true))
       .expand((interface) => interface.addresses).toList(growable: false);
