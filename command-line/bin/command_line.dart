28119145a11a28fe031e6cada9da3ffd5d4e1b83
Tambapps <tambapps@gmail.com>
2021-06-20 23:49:37 +0200

    initialized user data

--- command-line/bin/command_line.dart
+++ command-line/bin/command_line.dart
@@ -7,6 +7,7 @@ import 'package:p2p_chat_core/p2p_chat_core.dart';
 const SERVER_ARG = 'server';
 const ADDRESS_ARG = 'address';
 const WITH_ARG = 'with';
+const USERNAME_ARG = 'username';
 
 late Chat chat;
 
@@ -14,13 +15,14 @@ void main(List<String> arguments) async {
   print('P2P Chat 0.0.1');
   var argResults = getArgs(arguments);
   var callback = (Message message) => print('${message.userData.username} at ${message.sentAt.hour}:${message.sentAt.second}:\n${message.text}');
+  final username = argResults[USERNAME_ARG] ?? 'anonymous';
 
   if (argResults[SERVER_ARG]) {
-    chat = await serverChat(callback, argResults[ADDRESS_ARG] != null ? await toAddress(argResults[ADDRESS_ARG]) : await getDesktopIpAddress());
+    chat = await serverChat(callback, argResults[ADDRESS_ARG] != null ? await toAddress(argResults[ADDRESS_ARG]) : await getDesktopIpAddress(), username);
   } else if (argResults[WITH_ARG] != null) {
-    chat = await clientChat(callback, await toAddress(argResults[WITH_ARG]));
+    chat = await clientChat(callback, await toAddress(argResults[WITH_ARG]), username);
   } else {
-    chat = await smartChat(callback, argResults[ADDRESS_ARG] != null ? await toAddress(argResults[ADDRESS_ARG]) : await getDesktopIpAddress());
+    chat = await smartChat(callback, argResults[ADDRESS_ARG] != null ? await toAddress(argResults[ADDRESS_ARG]) : await getDesktopIpAddress(), username);
   }
 
   // dart is single threaded. If I would have processed the lines synchronously (e.g with stdin.readLineSync())
@@ -30,34 +32,36 @@ void main(List<String> arguments) async {
   });
 }
 
-Future<Chat> clientChat(MessageCallback messageCallback, InternetAddress address) async {
-  print('Connecting to $address');
-  var chat = await ChatClient.from(address, messageCallback);
+Future<Chat> clientChat(MessageCallback messageCallback, InternetAddress address, String username) async {
+  print('Connecting to ${address.address}');
+  var chat = await ChatClient.from(address, messageCallback, userData: UserData(username));
   print('Connected successfully');
   print("Tap text and press 'Enter' to send a message");
   return chat;
 }
 
-Future<Chat> serverChat(MessageCallback messageCallback, InternetAddress address) async {
-  var chatServer = await ChatServer.from(address, messageCallback,
+Future<Chat> serverChat(MessageCallback messageCallback, InternetAddress address, String username) async {
+  var chatServer = await ChatServer.from(address, messageCallback, userData: UserData(username),
       onNewSocket: (chat, user) {
-        print('$user connected!');
+        print('${user.username} connected!');
         print("Tap text and press 'Enter' to send a message");
         return true;
       });
   chatServer.start();
   print('Server started on ${chatServer.address.address}.\nWaiting on a connection...');
-  await multicast(ChatPeer.from(address, PeerType.SERVER, ChatServer.PORT));
+  var multicaster = await ChatPeerMulticaster.newInstance();
+  multicaster.chatPeers = [ chatServer.chatPeer ];
+  multicaster.start();
   return chatServer;
 }
 
-Future<Chat> smartChat(MessageCallback messageCallback, InternetAddress address) async {
+Future<Chat> smartChat(MessageCallback messageCallback, InternetAddress address, String username) async {
   print('Looking/waiting for another chat peer');
   var chat = await SmartChat.from(address, messageCallback, onNewSocket: (chat, user) {
     if (chat is ChatServer) {
-      print('$user connected to your chat!');
+      print('${user.username} connected to your chat!');
     } else {
-      print("Connected to $user's chat!");
+      print("Connected to ${user.username}'s chat!");
     }
     print("Tap text and press 'Enter' to send a message");
     return true;
@@ -66,19 +70,11 @@ Future<Chat> smartChat(MessageCallback messageCallback, InternetAddress address)
   return chat;
 }
 
-Future<ChatPeerMulticaster> multicast(ChatPeer chatPeer) async {
-  var multicaster = await ChatPeerMulticaster.newInstance();
-  multicaster.chatPeers = [
-    chatPeer
-  ];
-  multicaster.start();
-  return multicaster;
-}
-
 ArgResults getArgs(List<String> arguments) {
   final argParser = ArgParser()
   ..addFlag(SERVER_ARG, abbr: 's', negatable: false, help: 'Option to tell if you are the chat host (the server peer)')
   ..addOption(ADDRESS_ARG, abbr: 'a', help: 'Address to use (for server only)')
-  ..addOption(WITH_ARG, abbr: 'w', help: 'Address of the chat host (if you are a client)');
+  ..addOption(WITH_ARG, abbr: 'w', help: 'Address of the chat host (if you are a client)')
+  ..addOption(USERNAME_ARG, abbr: 'u', help: 'Username for the chat');
   return argParser.parse(arguments);
 }
