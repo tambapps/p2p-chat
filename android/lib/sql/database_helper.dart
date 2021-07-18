51c86bb7366b44a2dd0709c791c265c045399062
Tambapps <tambapps@gmail.com>
2021-07-19 00:52:52 +0200

    fixed tables creation

--- android/lib/sql/database_helper.dart
+++ android/lib/sql/database_helper.dart
@@ -9,29 +9,33 @@ class DatabaseHelper {
 
   DatabaseHelper(this.db);
 
+  static void createDatabases(Database db) async {
+    db.execute('CREATE TABLE users(id VARCHAR(255) PRIMARY KEY, name TEXT NOT NULL)');
+    db.execute('CREATE TABLE conversations(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255))');
+    db.execute('CREATE TABLE messages(id INTEGER PRIMARY KEY AUTOINCREMENT,'
+            'conversation_id INTEGER NOT NULL,'
+            'user_id VARCHAR(255) NOT NULL,'
+            'type VARCHAR(32) NOT NULL,'
+            'data LONGBLOB NOT NULL,'
+            'sent_at DATETIME NOT NULL,'
+            'CONSTRAINT messages_fk_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,'
+            'CONSTRAINT messages_fk_conversation_id FOREIGN KEY (conversation_id) REFERENCES conversations(id) ON DELETE CASCADE ON UPDATE CASCADE'
+            ')');
+  }
+
   static Future<DatabaseHelper> newInstance() async {
+    final databasePath = join(await getDatabasesPath(), 'p2p_chat_database.db');
+    // use the following line in case schema changes
+    // deleteDatabase(databasePath);
     final database = await openDatabase(
       // Set the path to the database. Note: Using the `join` function from the
       // `path` package is best practice to ensure the path is correctly
       // constructed for each platform.
-      join(await getDatabasesPath(), 'p2p_chat_database.db'),
+      databasePath,
       // When the database is first created, create a table to store dogs.
       onCreate: (db, version) {
         // Run the CREATE TABLE statement on the database.
-        return db.execute(
-          'CREATE TABLE users(id VARCHAR(255) PRIMARY KEY, name TEXT NOT NULL);' +
-              'CREATE TABLE conversations(id INTEGER PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255));' +
-              'CREATE TABLE messages(id INTEGER PRIMARY KEY AUTO_INCREMENT,'
-                  'conversation_id INTEGER NOT NULL,'
-                  'user_id VARCHAR(255) NOT NULL,'
-                  'type VARCHAR(32) NOT NULL,'
-                  'data LONGBLOB NOT NULL,'
-                  'sent_at DATETIME NOT NULL,'
-                  'CONSTRAINT messages_fk_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,'
-                  'CONSTRAINT messages_fk_conversation_id FOREIGN KEY (conversation_id) REFERENCES conversations(id) ON DELETE CASCADE ON UPDATE CASCADE'
-                  ');' +
-              '',
-        );
+        return createDatabases(db);
       },
       // Set the version. This executes the onCreate function and provides a
       // path to perform database upgrades and downgrades.
