3e1cb5c42cc77dc13c145391ec3289dd4200989e
Tambapps <tambapps@gmail.com>
2021-10-24 01:34:02 +0200

    updated color

--- android/lib/constants.dart
+++ android/lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF00BF6D);
-const kSecondaryColor = Color(0xFFFE9901);
+const kSecondaryColor = Color(0xFF00FFFF);
 const kContentColorLightTheme = Color(0xFF1D1D35);
 const kContentColorDarkTheme = Color(0xFFF5FCF9);
 const kWarninngColor = Color(0xFFF3BB1C);
