575af0165a0835c6cfe7b95f49888bae619b1fcf
Tambapps <tambapps@gmail.com>
2021-10-24 11:34:32 +0200

    fixed app bar title color

--- android/lib/main.dart
+++ android/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final appBarTheme = AppBarTheme(centerTitle: false, elevation: 0);
+    final appBarTheme = AppBarTheme(centerTitle: false, elevation: 0, color: kPrimaryColor);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark().copyWith(
