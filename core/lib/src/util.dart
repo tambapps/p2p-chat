3023a2ecbd0e60f6bf21366e0228049691cd1dc2
Tambapps <tambapps@gmail.com>
2021-10-30 23:13:13 +0200

    formated month myself

--- core/lib/src/util.dart
+++ core/lib/src/util.dart
@@ -52,17 +52,44 @@ Future<InternetAddress> getDesktopIpAddress() async {
   return addresses[0];
 }
 
-final _MONTH_FORMAT = DateFormat("MMM");
+String formatMonth(int month) {
+  switch (month) {
+    case 1:
+      return 'Jan';
+    case 2:
+      return 'Feb';
+    case 3:
+      return 'Mar';
+    case 4:
+      return 'Apr';
+    case 5:
+      return 'May';
+    case 6:
+      return 'Jun';
+    case 7:
+      return 'Jul';
+    case 8:
+      return 'Aug';
+    case 9:
+      return 'Sep';
+    case 10:
+      return 'Oct';
+    case 11:
+      return 'Nov';
+    case 12:
+      return 'Dec';
+    default:
+      return '???';
+  }
+}
 
-// TODO remove intl dependency and format month manually
 String formatDate(DateTime dateTime) {
   final now = DateTime.now();
-  final yesterday = DateTime.now().subtract(Duration(days: 1));
   String time = _twoDigitsNumber(dateTime.hour) + ":" + _twoDigitsNumber(dateTime.minute);
   if (_isSameDay(dateTime, now)) {
     return _twoDigitsNumber(dateTime.hour) + ":" + _twoDigitsNumber(dateTime.minute);
   }
-  String monthAndDay =  dateTime.day.toString() + " " + _MONTH_FORMAT.format(dateTime) + ".";
+  String monthAndDay =  dateTime.day.toString() + " " + formatMonth(dateTime.month) + ".";
   if (_isSameYear(dateTime, now)) {
     return monthAndDay + " " + time;
   }
